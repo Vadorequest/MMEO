ebc61f952fea01fc65a26d5d5a61eb69f750a076
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-02-16 14:09:23 +0100

    Upload MP3 constants

--- constants/Sounds.js
+++ constants/Sounds.js
@@ -2,4 +2,11 @@ export default {
   backgroundSample: require(`../assets/sounds/SampleAudio_0.4mb.mp3`),
   successLong: require(`../assets/sounds/success_long.mp3`),
   successShort: require(`../assets/sounds/success_short.mp3`),
+  metal1: require(`../assets/sounds/audio_metal.mp3`),
+  metal2: require(`../assets/sounds/audio_metal_2.mp3`),
+  boxing1: require(`../assets/sounds/audio_boxing_1.mp3`),
+  boxing2: require(`../assets/sounds/audio_boxing_2.mp3`),
+  movement: require(`../assets/sounds/audio_mouvement.mp3`),
+  taille: require(`../assets/sounds/audio_taille.mp3`),
+  vague: require(`../assets/sounds/audio-vague.mp3`),
 };
